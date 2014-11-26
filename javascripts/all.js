0e544073cb229f887405e2302f1f96e0adf7f336
Jim Van Fleet <jim@jimvanfleet.com>
2014-11-26 06:55:00 -0500

    Site updated to da38a22

--- javascripts/all.js
+++ javascripts/all.js
@@ -201,33 +201,6 @@ window.jQuery = window.$ = jQuery;
 
 /* Custom Scripts */
 
-/*-----------------------------------------------------------------------------------*/
-/*	CONTACT FORM
-/*-----------------------------------------------------------------------------------*/
-jQuery(document).ready(function() {
-	$("#ajax-contact-form").submit(function() {
-		var str = $(this).serialize();		
-		$.ajax({
-			type: "POST",
-			url: "contact_form/contact_process.php",
-			data: str,
-			success: function(msg) {
-				// Message Sent - Show the 'Thank You' message and hide the form
-				if(msg == 'OK') {
-					result = '<div class="notification_ok">Your message has been sent. Thank you!</div>';
-					$("#fields").hide();
-				} else {
-					result = msg;
-				}
-				$('#note').html(result);
-			}
-		});
-		return false;
-	});
-});
-
-
-
 
 /*-----------------------------------------------------------------------------------*/
 /*	MENU
@@ -323,14 +296,6 @@ $(document).ready(function() {
 	}); 
 });
 
-
-
-/*-----------------------------------------------------------------------------------*/
-/*	PRETTYPHOTO
-/*-----------------------------------------------------------------------------------*/
-jQuery(document).ready(function() {
-	jQuery("a[rel^='prettyPhoto']").prettyPhoto();
-});
 
 /*-----------------------------------------------------------------------------------*/
 /*	FLICKR
